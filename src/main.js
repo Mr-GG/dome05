f7edf92d3a4f77685d064a50fb3bf3017ce24a7b
Mr-GG <2392121983@qq.com>
2020-08-26 22:42:51 +0800

    加入vant

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/base.less'
+import './styles/iconfont.css'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+Vue.use(Vant)
+// import { Button } from 'vant'
+// Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>我是登录页面</div>
+  <div>我是登录页面
+      <span class="iconfont icondianzan"></span>
+      <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
